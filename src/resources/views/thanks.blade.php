a7eb868acc9da50318c3965878dcbb9b026b0086
haruka wada <022.wadaha@gmail.com>
2024-08-09 16:31:02 +0900

    thanksページのcss追加

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -1,1 +1,25 @@
-<p>お問い合わせありがとうございました</p>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FashionablyLate</title>
+    <link rel="stylesheet" href="{{ asset('/css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="thanks-container">
+        <p class="thanks-background">Thank you</p>
+        <p class="thanks-text">お問い合わせありがとうございました</p>
+        <div class="home-button">
+            <a href="/">HOME</a>
+        </div>
+    </div>
+</body>
+
+</html>
